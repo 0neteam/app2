4e8dcd65196af5e39e13d198f0095cddad489bb0
KY129 <dlruddlf129@naver.com>
2025-02-06 19:48:08 +0900

    docs: html 파일 수정-m
    transpEmail.html 수정완료

--- src/main/java/com/java/transp/TranspController.java
+++ src/main/java/com/java/transp/TranspController.java
@@ -1,16 +1,20 @@
-//package com.java.transp;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//
-//@Controller
-//public class TranspController {
-//
-//    // 운송 진행 현황 페이지 반환
-//    @GetMapping("/transp")
-//    public String showTransportPage() {
-//        return "transp";  // transp.html 템플릿 반환
-//    }
-//
-//}
+package com.java.transp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class TranspController {
+
+    @GetMapping("/transpEmail")
+    public String TranspEmail() {
+        return "transp/transpEmail";
+    };
+    
+    @GetMapping("/transp")
+    public String Transp() {
+        return "transp/transp";
+    };
+
+}
