aa782ec5405db745745a56577992637a2780f1da
J.Lim <j.lim9304@gmail.com>
2025-02-06 15:41:04 +0900

    docs: 코드 추가
    DTO 코드 추가

--- src/main/java/com/java/quo/QuoDTO.java
+++ src/main/java/com/java/quo/QuoDTO.java
@@ -1,5 +1,28 @@
 package com.java.quo;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class QuoDTO {
 	
+	private int quoNo;
+	private int orderNo;
+	private int bizNo;
+	private Date quoDate;
+	private String quoStatus;
+	private int qty;
+	private String departure;
 }
